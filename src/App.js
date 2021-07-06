4b03dc138980d30c204b9883bd8fee0d903261a1
Sachin Nishad <nishadsachin8898@gmail.com>
2021-07-07 02:13:50 +0530

    included firebase util, created methods to store user data into firebase

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/header/header.component';
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import { auth } from './firebase/firebase.utils';
+import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 
 class App extends React.Component {
 
@@ -16,15 +16,30 @@ class App extends React.Component {
     }
   }
 
-  unSubscribeFromAuth = null;
+  unSubscribeFromAuth =  null;
 
   componentDidMount(){
-    this.unSubscribeFromAuth = auth.onAuthStateChanged(user => {
-      this.setState({currentUser: user});
-      console.log(user);
+    this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
+      if(userAuth){
+        const userRef = await createUserProfileDocument(userAuth);
+        userRef.onSnapshot(snapShot => {
+          this.setState(
+            {
+              currentUser: {
+                id:snapShot.id,
+                ...snapShot.data()
+              }
+            }
+          );
+        })
+      } else{
+        this.setState({
+          currentUser:userAuth
+        });
+      }
     })
   }
-
+    
   componentWillUnmount(){
     this.unSubscribeFromAuth();
   }
