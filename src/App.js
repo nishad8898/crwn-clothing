115d096241d12ae36e9b984845b534f6c5acd5ca
Sachin Nishad <nishadsachin8898@gmail.com>
2021-07-11 20:42:21 +0530

    added redux, and implemented userReducer and userAction to header and app components

--- src/App.js
+++ src/App.js
@@ -6,48 +6,39 @@ import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import SignInAndSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
+import { connect } from 'react-redux';
+import { setCurrentUser } from './redux/user/user.actions';
 
 class App extends React.Component {
-
-  constructor(){
-    super();
-    this.state = {
-      currentUser: null
-    }
-  }
 
   unSubscribeFromAuth =  null;
 
   componentDidMount(){
+    const { setCurrentUser } = this.props;
     this.unSubscribeFromAuth = auth.onAuthStateChanged(async userAuth => {
       if(userAuth){
         const userRef = await createUserProfileDocument(userAuth);
         userRef.onSnapshot(snapShot => {
-          this.setState(
-            {
-              currentUser: {
+          setCurrentUser({
                 id:snapShot.id,
                 ...snapShot.data()
               }
-            }
           );
         })
       } else{
-        this.setState({
-          currentUser:userAuth
-        });
+        setCurrentUser(userAuth);
       }
     })
   }
-    
+
   componentWillUnmount(){
     this.unSubscribeFromAuth();
   }
-  
+
   render(){
     return (
       <div>
-        <Header currentUser = {this.state.currentUser} />
+        <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/shop' component={ShopPage} />
@@ -56,7 +47,10 @@ class App extends React.Component {
       </div>
     );
   }
-
 }
 
-export default App;
+const mapDispatchToProps = dispatch => ({
+  setCurrentUser: user => dispatch(setCurrentUser(user))
+});
+
+export default connect(null, mapDispatchToProps)(App);
--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -54,7 +54,7 @@ class SignIn extends React.Component{
 
                     <div className='buttons'>
                         <CustomButton type="submit" >sign in</CustomButton>
-                        <CustomButton onClick = {signInWithGoogle} isGoogleSignIn >sign in with google
+                        <CustomButton type="button" onClick = {signInWithGoogle} isGoogleSignIn >sign in with google
                         </CustomButton> 
                     </div>
                 </form>
